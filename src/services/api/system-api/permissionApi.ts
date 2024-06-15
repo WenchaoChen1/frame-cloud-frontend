517639df11d0dab8b59f8facde2d903fcc2ce1d3
wenchao <wenchao@goldensection.com>
2024-06-15 17:29:31 +0800

    no message

--- src/services/api/system-api/permissionApi.ts
+++ src/services/api/system-api/permissionApi.ts
@@ -4,7 +4,7 @@ export async function getPermissionList(
   params: APIIdentity.Pager,
   options?: { [key: string]: any },
 ) {
-  return request<any>(`${process.env.SYSTEM_SERVICE}/v1/permission/page`, {
+  return request<any>(`${process.env.SYSTEM_SERVICE}/v1/permission/get-permission-manage-page`, {
     method: 'GET',
     params: {
       ...params,
@@ -19,6 +19,12 @@ export async function getPermissionType() {
   });
 }
 
+export async function insertAndUpdatePermissionManage(data: APISystem.PermissionItemDataType) {
+  return request<APISystem.OrganizeListResponseDataType>(`${process.env.SYSTEM_SERVICE}/v1/permission/insert-and-update-permission-manage`, {
+    method: 'POST',
+    data: data,
+  });
+}
 export async function savePermission(data: APISystem.PermissionItemDataType) {
   return request<APISystem.OrganizeListResponseDataType>(`${process.env.SYSTEM_SERVICE}/v1/permission`, {
     method: 'POST',
--- src/services/system-service/permissionService.ts
+++ src/services/system-service/permissionService.ts
@@ -1,4 +1,10 @@
-import {deletePermissionById, getPermissionList, getPermissionType, savePermission} from "@/services/api/system-api/permissionApi";
+import {
+  deletePermissionById,
+  getPermissionList,
+  getPermissionType,
+  insertAndUpdatePermissionManage,
+  savePermission
+} from "@/services/api/system-api/permissionApi";
 
 export async function getPermissionListService(params: APIIdentity.Pager) {
   return await getPermissionList(params);
@@ -9,7 +15,7 @@ export async function getPermissionTypeService() {
 }
 
 export async function savePermissionService(data: APISystem.PermissionItemDataType) {
-  return await savePermission(data);
+  return await insertAndUpdatePermissionManage(data);
 }
 
 export async function deletePermissionByIdService(id: string) {
