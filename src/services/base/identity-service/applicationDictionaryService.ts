e3aa01497fdb30f25ce7cf8681702c726f75e83c
Yue zhang <yue.zhang@goldensection.com>
2024-06-20 17:24:48 +0800

    bug

--- src/services/base/identity-service/applicationDictionaryService.ts
+++
@@ -1,7 +0,0 @@
-import {
-  getAuthorizationGrantTypes,
-} from "@/services/api/identity-api/applicationDictionaryApi";
-
-export async function getAuthorizationGrantTypesService() {
-  return await getAuthorizationGrantTypes();
-}
--- src/services/base/identity-service/complianceService.ts
+++
@@ -1,8 +0,0 @@
-import {
-  getComplianceManagePage,
-} from "@/services/api/identity-api/complianceApi";
-
-
-export async function getComplianceManagePageService(params?: any) {
-  return await getComplianceManagePage(params);
-}
