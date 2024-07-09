b14d3948281fda8cfec3a60a9a481b675a7f24aa
Yue zhang <yue.zhang@goldensection.com>
2024-07-09 15:49:46 +0800

    bug

--- src/access.ts
+++ src/access.ts
@@ -16,34 +16,3 @@ export default function access(initialState: { currentUser?: any } | undefined) 
 
   return jurisdictions;
 }
-
-// const estimate = (initialState) =>{
-//   let authorityList:[] = []
-//   let userRouters = null
-//   if (initialState?.currentUser){
-//     userRouters = initialState?.currentUser?.leftAndTopRoutes
-//   }
-//   const recursion = (datas: any) =>{
-//     datas?.forEach((item: any)=>{
-//       if (item.name){
-//         authorityList.push(item?.name)
-//       }
-//       if (item?.children && item?.children.length > 0){
-//         recursion(item.children)
-//       }
-//     })
-//   }
-//   recursion(userRouters)
-//   return authorityList
-// }
-
-// export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-//   const jurisdictions = {}
-//   const {permissionLists} = permissions
-//   const powerLists = estimate(initialState)
-//   for (const Key in permissionLists) {
-//     jurisdictions[Key] = powerLists?.includes(permissionLists[Key])
-//   }
-//   console.log(jurisdictions, '******')
-//   return jurisdictions;
-// }
