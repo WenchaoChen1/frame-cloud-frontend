41279d936983a026d0940910411a44cba607af3e
wenchao <wenchao@goldensection.com>
2024-07-13 15:44:58 +0800

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//   extends: [require.resolve('@umijs/lint/dist/config/eslint')],
+//   globals: {
+//     page: true,
+//     REACT_APP_ENV: true,
+//   },
+// };
+// .eslintrc.js
 module.exports = {
-  extends: [require.resolve('@umijs/lint/dist/config/eslint')],
-  globals: {
-    page: true,
-    REACT_APP_ENV: true,
-  },
-};
+  // Umi 项目
+  extends: require.resolve('umi/eslint'),
+
+  // Umi Max 项目
+  extends: require.resolve('@umijs/max/eslint'),
+}
